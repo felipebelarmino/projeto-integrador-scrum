921b8c0333a332a867c06da966f1cbdf042f93f1
Natasha Gulyas <natashagulyas@gmail.com>
2021-05-10 16:05:50 -0300

    inclusão de colunas faltantes em user e delete da Store e Admin

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,7 @@ import Sequelize from "sequelize";
 import databaseConfig from "../config/database"; //importar credenciais
 import User from "../app/models/User";
 import FileModel from "../app/models/FileModel";
-import AdminModel from "../app/models/AdminModel";
 import Product from "../app/models/Product";
-import Store from "../app/models/StoreModel";
 import CategoryModel from "../app/models/CategoryModel";
 import AddressModel from "../app/models/AddressModel";
 import Order from "../app/models/Order";
@@ -12,8 +10,6 @@ import OrderProduct from "../app/models/OrderProduct";
 
 const models = [
   User,
-  AdminModel,
-  Store,
   Product,
   CategoryModel,
   FileModel,
